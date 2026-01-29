c32e9e96e879aa4c9413c11513ba53e967a39914
TinhVan2809 <tinhlu703@gmail.com>
2026-01-29 19:06:22 +0700

    Update dashbroad admin

--- frontend/src/pages/admin/Dashbroad.jsx
+++ frontend/src/pages/admin/Dashbroad.jsx
@@ -1,6 +1,8 @@
+// Trang Dashbroad admin
 function Dashbroad() {
     return (
         <>
+        
             <h1 className="text-3xl font-bold">Bảng điều khiển</h1>
             <p className="mt-4">Chào mừng đến với trang quản trị!</p>
         </>
